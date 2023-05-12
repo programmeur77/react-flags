2d1845c01cc446582a71e7f56d74453484cc9fd3
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-05-12 09:18:21 +0200

    Creation of a className as per link activation or not

--- flags/src/components/Navigation.jsx
+++ flags/src/components/Navigation.jsx
@@ -4,10 +4,13 @@ const Navigation = () => {
   return (
     <div className="navigation">
       <ul>
-        <Link to={`/`}>
+        <Link to={`/`} className={(nav) => (nav.isActive ? '.nav-active' : '')}>
           <li>Accueil</li>
         </Link>
-        <Link to={`/about`}>
+        <Link
+          to={`/about`}
+          className={(nav) => (nav.isActive ? '.nav-active' : '')}
+        >
           <li>À propos</li>
         </Link>
       </ul>
