6d31621ed35984ee95900c52ac7deca6cb0fbaae
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-05-12 19:59:44 +0200

    Initialization of file and coded everything regarding this component

---
+++ flags/src/components/Card.jsx
@@ -0,0 +1,20 @@
+const Card = ({ country }) => {
+  console.log(country);
+  return (
+    <div>
+      <li className="card">
+        <img
+          src={country.flags.svg}
+          alt={'drapeau ' + country.translations.fra.common}
+        />
+        <div className="infos">
+          <h2>{country.translations.fra.common}</h2>
+          <h4>{country.capital}</h4>
+          <p>Pop. {country.population.toLocaleString()}</p>
+        </div>
+      </li>
+    </div>
+  );
+};
+
+export default Card;
